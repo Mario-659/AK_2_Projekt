0a176ec729dd12219b268586cbaf46e776a17e0d
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-04-01 15:11:17 +0200

    add sub

--- main.cpp
+++ main.cpp
@@ -18,15 +18,34 @@ unsigned char bcd_add(unsigned char x, unsigned char y) {
    return x;
 }
 
+unsigned char bcd_sub(unsigned char x, unsigned char y) {
+   asm(R"(
+            movb %0, %%al      # x -> AL
+            movb %1, %%bl      # y -> BL
+            sub %%bl, %%al     # x - y -> AL
+            das                # Decimal Adjust AL after Subtraction
+            movb %%al, %0      # al -> x
+      )" 
+      : "+r"(x)
+      : "r"(y)
+      : "%al", "%bl"
+   );
+
+   return x;
+}
+
 int main() {
-   unsigned char x = 0x54;  // Liczba 54 zakodowana w BCD
+   unsigned char x = 0x21;  // Liczba 54 zakodowana w BCD
    unsigned char y = 0x23;  // Liczba 23 zakodowana w BCD
 
    unsigned char result = bcd_add(x, y); // Wynik dodawania
 
    
    //Wyswietlenie wyniku zakodowanego w BCD
-   cout << "Result: " << hex << (int)result << endl;
+   cout << "Result of addition: " << hex << (int)result << endl;
+   
+   result = bcd_sub(x, y); // Wynik odejmowania
+   cout << "Result of substraction: " << hex << (int)result << endl;
 
    return 0;
 }
